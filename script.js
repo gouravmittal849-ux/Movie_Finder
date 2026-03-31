7e74a603c753da179541136f98c2ec4f1d125063
gouravmittal849-ux <gouravmittal849@gmail.com>
2026-03-31 14:33:08 +0530

    Initial commit: Movie Finder application

---
+++ script.js
@@ -0,0 +1,21 @@
+const moviesDiv = document.getElementById("movies");
+const loading = document.getElementById("loading");
+
+async function fetchMovies() {
+    loading.style.display = "block";
+
+    const res = await fetch("https://www.omdbapi.com/?apikey=564727fa&s=batman");
+    const data = await res.json();
+
+    loading.style.display = "none";
+
+    moviesDiv.innerHTML = data.Search.map(movie => `
+        <div class="card">
+            <img src="${movie.Poster}">
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+        </div>
+    `).join("");
+}
+
+fetchMovies();
